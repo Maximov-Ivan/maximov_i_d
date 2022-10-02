7399ee7b2fa91d17296825ba3389a0eb4aa479c4
Maximov-Ivan <113625972+Maximov-Ivan@users.noreply.github.com>
2022-10-02 11:42:00 +0300

    Update 1352a.cpp

--- 1352a.cpp
+++ 1352a.cpp
@@ -6,7 +6,7 @@ int main() {
     int n = 0;
     std::cin >> t;
     std::vector <std::vector <int> > a(t);
-    for (int i = 0; i < t; ++i) {
+    for (int i = 0; i < t; i += 1) {
         std::cin >> n;
         if (n / 10000 != 0) {
             a[i].push_back((n / 10000) * 10000);
@@ -20,9 +20,9 @@ int main() {
             a[i].push_back(n % 10);
         }
     }
-    for (int i = 0; i < t; ++i) {
+    for (int i = 0; i < t; i += 1) {
         std::cout << a[i].size() << "\n";
-        for (int j = 0; j < a[i].size(); ++j) {
+        for (int j = 0; j < a[i].size(); j += 1) {
             std::cout << a[i][j] << ' ';
         }
         std::cout << "\n";
