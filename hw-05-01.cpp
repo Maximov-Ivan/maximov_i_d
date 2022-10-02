f9875fda03399e81e0faceb5f289cd812ebd19d9
Maximov-Ivan <113625972+Maximov-Ivan@users.noreply.github.com>
2022-10-02 13:26:03 +0300

    Create hw-05-01.cpp

---
+++ hw-05-01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+double summ(double& x, double& eps) {
+    double e = 1; // элемент, первый элемент равен 1
+    double s = 1; // сумма, начальная сумма равноа первому элементу
+    int i = 0; // индекс
+    while (std::abs(e) > eps) {
+        i += 1;
+        e *= (-1) * x * x / (2 * i - 1) / (2 * i);
+        s += e;
+    }
+    return s;
+}
+
+int main() {
+    double a = -1;
+    double b = 1;
+    double delta = 0.1;
+    double eps = 0.001;
+    for (double x = a; x <= b; x += delta) {
+        double s = summ(x, eps);
+        double f = cos(x);
+        std::cout.precision(1);
+        std::cout.width(10);
+        std::cout << x;
+        std::cout << ' ';
+        std::cout.precision(7);
+        std::cout.width(10);
+        std::cout << s;
+        std::cout << ' ';
+        std::cout.precision(7);
+        std::cout.width(10);
+        std::cout << f;
+        std::cout << "\n";
+    }
+}
