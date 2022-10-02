460a871ac75f716b9fb83570cc0d765c94962e41
Maximov-Ivan <113625972+Maximov-Ivan@users.noreply.github.com>
2022-10-02 11:24:33 +0300

    Create 0271a.cpp

---
+++ 0271a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    int y = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    std::cin >> y;
+    for (y++; y < 10000; ++y) {
+        a = y / 1000;
+        b = y / 100 % 10;
+        c = y / 10 % 10;
+        d = y % 10;
+        if (a != b && a != c && a != d && b != c && b != d && c != d) {
+            std::cout << y;
+            break;
+        }
+    }
+    return 0;
+}
